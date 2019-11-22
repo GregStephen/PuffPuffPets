d24e5a58c943796967e6be2ac7004d9625ce8c0b
Greg Stephen <GregAStephen@gmail.com>
2019-11-22 10:10:36 -0600

    got the Get and Post set but need to talk about what the delete does

---
+++ PuffPuffPets.Api/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PuffPuffPets.Api.DataModels;
+using PuffPuffPets.Api.Dtos;
+using PuffPuffPets.Api.Repositories;
+
+namespace PuffPuffPets.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserRepository _repo;
+
+        public UserController(ILogger<UserController> logger, IUserRepository repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetAll()
+        {
+            return _repo.GetAllUsers();
+        }
+
+        [HttpGet("{userId}")]
+        public User GetUser(Guid userId)
+        {
+            return _repo.GetUserById(userId);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(AddUserDto newUser)
+        {
+            if (_repo.AddNewUser(newUser))
+            {
+                return Created($"user/{newUser.FirstName}", newUser);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
--- PuffPuffPets.Api/Repositories/UserRepository.cs
+++ PuffPuffPets.Api/Repositories/UserRepository.cs
@@ -1,11 +1,67 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using PuffPuffPets.Api.DataModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using PuffPuffPets.Api.Dtos;
 
 namespace PuffPuffPets.Api.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        string _connectionString = "Server=localhost;Database=PuffPuffPets;Trusted_Connection=True;";
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var users = db.Query<User>(@"SELECT * FROM [User]");
+                return users;
+            }
+        }
+
+        public User GetUserById(Guid userId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"SELECT *
+                            FROM [User]
+                            WHERE [Id] = @userId";
+                var parameters = new { userId };
+                var user = db.QueryFirst<User>(sql, parameters);
+                return user;
+            }
+        }
+
+        public bool AddNewUser(AddUserDto newUser)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                // needs to do a check to see if the username already exists
+                // needs to do a check to see if the email already exits
+                var sql = @"
+                            INSERT INTO [User]
+                                ([IsSeller],
+                                 [UserName],
+                                 [FirstName],
+                                 [LastName],
+                                 [Email],
+                                 [DateCreated],
+                                 [Password],
+                                 [BusinessName])
+                            VALUES
+                                ([@isSeller],
+                                 [@userName],
+                                 [@firstName],
+                                 [@lastName],
+                                 [@email],
+                                 [@dateCreated],
+                                 [@password],
+                                 [@businessName])";
+                return db.Execute(sql, newUser) == 1;
+            }
+        }
     }
 }
