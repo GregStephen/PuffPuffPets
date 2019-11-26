a1c75cadba1ec51c5a9595c75f0fda01502735f6
Greg Stephen <GregAStephen@gmail.com>
2019-11-25 18:49:23 -0600

    did category and product type get all calls

---
+++ PuffPuffPets.Api/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using PuffPuffPets.Api.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuffPuffPets.Api.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        string _connectionString = "Server=localhost;Database=PuffPuffPets;Trusted_Connection=True;";
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var categories = db.Query<Category>(@"SELECT * FROM [Category]");
+                return categories;
+            }
+        }
+    }
+}
