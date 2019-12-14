fa044e90c2a031b92bb591f66d2dce2668023675
David Mullins <Mullins6323@gmail.com>
2019-12-13 23:14:58 -0600

    Total cost per item displaying. Added product descriptions in SQL

--- puffpuffpets.ui/src/Components/CartCard/CartCard.js
+++ puffpuffpets.ui/src/Components/CartCard/CartCard.js
@@ -11,7 +11,8 @@ const defaultQuantityOrdered = {
 
 class CartCard extends React.Component {
   state = {
-    newQuantityOrdered: defaultQuantityOrdered
+    newQuantityOrdered: defaultQuantityOrdered,
+    price: this.props.cartProduct.price * this.props.cartProduct.quantityOrdered
   }
 
   deleteMe = (e) => {
@@ -24,14 +25,29 @@ class CartCard extends React.Component {
     .catch(err => console.error(err, 'unable to delete from CartCard'));
   }
 
+  //Passes productOrderId and new quantityOrdered to state.
+  //Update is sent to the backend using componentDidUpdate.
+  //Update to total price on frontend only. 
+  //Total price update is done in the final checkout stage to avoid additional api call here.
   formFieldStringState = (e) => {
     const tempQuantityOrdered = { ...this.state.newQuantityOrdered };
+    const { cartProduct } = this.props;
+
     tempQuantityOrdered.quantityOrdered = parseInt(e.target.value, 10);
     tempQuantityOrdered.id = this.props.cartProduct.productOrderId;
-    this.setState({ newQuantityOrdered: tempQuantityOrdered });
+    this.setState({ 
+    newQuantityOrdered: tempQuantityOrdered,
+    price: (cartProduct.price * tempQuantityOrdered.quantityOrdered)
+    });
   }
 
   updateQuantityOrdered = e => this.formFieldStringState(e);
+
+  //Sets default value of quantityOrdered-input as user's last updated amount; sets value as 1 if user has not updated.
+  checkQuantityInCart = () => {
+    const { quantityOrdered } = this.props.cartProduct;
+    return (quantityOrdered > 1 ? quantityOrdered : 1);
+  }
 
   componentDidUpdate() {
     const { newQuantityOrdered } = this.state;
@@ -39,6 +55,7 @@ class CartCard extends React.Component {
   }
 
   componentDidMount() {
+    //Deletes product from cart if the seller no longer has any in stock.
     if (this.props.cartProduct.quantityInStock === 0) {
       this.deleteMe();
     }
@@ -53,14 +70,14 @@ class CartCard extends React.Component {
             <div className="row">
               <img className="productImg" src={cartProduct.imgUrl} alt="product"></img>
               <h3 className="col card-title">{cartProduct.title}</h3>
-              <h4 className="col card-price">Total: <i>${cartProduct.totalPrice}</i></h4>
+              <h4 className="col card-price">Total: <i>${this.state.price}</i></h4>
               <button className="col btn btn-outline-danger mx-auto removeFromCart" onClick={this.deleteMe}><b>Remove <br></br>From Cart</b></button>
             </div>
             <div className="row">
               <p className="col quantityOrderedText">Quantity:</p> 
-              <input className="col quantityOrderedInput" type="number" onChange={this.updateQuantityOrdered} defaultValue="1" min="0" max={cartProduct.quantityInStock}></input>
+              <input className="col quantityOrderedInput" type="number" onChange={this.updateQuantityOrdered} defaultValue={this.checkQuantityInCart()} min="0" max={cartProduct.quantityInStock}></input>
               <p className="col-4 cartProductDescription"><i>{cartProduct.description}</i></p>
-              <p className="col-4 cartProductDescription"><u>Category</u><br></br><i>{cartProduct.name}</i></p>
+              <p className="col cartProductCategory"><u>Category</u><br></br><i>{cartProduct.name}</i></p>
             </div>
           </div>
         </div>
