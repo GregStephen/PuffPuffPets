81cb14c6416392743f05fb8e5be50e32d2339cdd
Calvin Foster <cal.j.foster@gmail.com>
2019-12-17 21:08:43 -0600

    another commit

--- puffpuffpets.ui/src/Components/SellerHome/SellerHome.js
+++ puffpuffpets.ui/src/Components/SellerHome/SellerHome.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import ProductCard from '../ProductCard/ProductCard';
-import ProductRequest from '../../Helpers/Data/ProductRequests';
 
 import './SellerHome.scss';
 
 class SellerHome extends Component {
   
   componentDidMount() {
-    const userId = this.props.userObj.id;
-    this.getMyProducts(userId);
+   
   }
   
   render () {
