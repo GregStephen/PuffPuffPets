5fe5fbb56b043de7ac0400a1f13a5bf601f4feac
Greg Stephen <GregAStephen@gmail.com>
2019-12-30 16:31:40 -0600

    shows the store name and the products the store sells

--- puffpuffpets.ui/src/Components/SellerStorePage/SellerStorePage.js
+++ puffpuffpets.ui/src/Components/SellerStorePage/SellerStorePage.js
@@ -1,19 +1,44 @@
 import React from 'react';
 
+import ProductCard from '../ProductCard/ProductCard';
+
+import UserRequests from '../../Helpers/Data/UserRequests';
+import ProductRequests from '../../Helpers/Data/ProductRequests';
+
 import './SellerStorePage.scss';
-
 class SellerStorePage extends React.Component{
-
+  state = {
+    seller: {},
+    sellersProducts: [],
+  }
   componentDidMount() {
     const { sellerId } = this.props.match.params;
-    console.error(this.props.match.params);
-    console.error(sellerId, 'seller Id');
+    UserRequests.getUserById(sellerId)
+      .then((sellerReturned) => {
+        this.setState({seller: sellerReturned})
+        ProductRequests.getProductsByUserId(sellerReturned.id)
+          .then((result) => {
+            this.setState({ sellersProducts: result });
+          }).catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
+    const {seller, sellersProducts} = this.state;
+    const showProducts = sellersProducts.map(product => (
+      <ProductCard 
+      key={product.id}
+      product={product}
+      userObj={this.props.userObj}
+      />
+    ))
     return (
-      <div className="SellerStorePage">
-        <h1>Seller Store Page</h1>
+      <div className="SellerStorePage container">
+        <h1>{seller.businessName} Store Page</h1>
+        <div className="row">
+          {showProducts}
+        </div>
       </div>
     )
   }
